3eec6ddf396290826e11c4d116d91b4def004ee2
lakshmi.sb <lakshmi.sb@SON15682.SSLTUIODC.LOCAL>
2017-04-11 11:38:09 +0530

    even

---
+++ src/test/java/MavenPackage/Even_Odd_Numbers.java
@@ -0,0 +1,24 @@
+package MavenPackage;
+
+public class Even_Odd_Numbers {
+							
+	public void ex()
+	{
+	
+	 for(int i=1;i<=100;i++)
+	 {
+	 if (i%2!=0)
+		 System.out.println("data" +i);
+	 }
+     	}
+	
+	public static void main(String[] args) 
+	
+	{
+ 
+		Even_Odd_Numbers Object=new Even_Odd_Numbers();
+		Object.ex();
+
+		
+	}
+}
